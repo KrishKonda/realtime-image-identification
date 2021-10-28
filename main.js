4a25b7e9ebee22bddfca32bcfa65f46e89e9f399
KrishKonda <77596750+KrishKonda@users.noreply.github.com>
2021-10-27 20:52:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
+function modelLoaded(){
+    console.log("Model is Loaded");
+}
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/g7zlAV5gK/model.json",modelLoaded);
 }
 function draw(){
-
+image(video,0,0,300,300);
+classifier.classify(video,gotResult);
+}
+function gotResult(error,results){
+    if (error) {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
 }
